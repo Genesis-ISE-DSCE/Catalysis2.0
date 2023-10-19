bc0264f0f7ccfab70bb9b7d7a1e9530bbb279b46
Aditya <code.adi8103@gmail.com>
2023-10-20 00:34:28 +0530

    committed the gallery section

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -1,9 +1,89 @@
+import React, { useState } from 'react';
+import A from '../assets/gallery/A.jpg';
+import B from '../assets/gallery/B.jpg';
+import C from '../assets/gallery/C.jpg';
+import D from '../assets/gallery/D.jpg';
+import E from '../assets/gallery/E.jpg';
+import F from '../assets/gallery/F.jpg';
+import G from '../assets/gallery/G.jpg';
+import H from '../assets/gallery/H.jpg';
+import I from '../assets/gallery/I.jpg';
+import J from '../assets/gallery/J.jpg';
+import K from '../assets/gallery/K.jpg';
+import V from '../assets/gallery/V.jpg';
+
+
+
 const Gallery = () => {
-    return(
-        <div id="gallery">
-            Gallery
-        </div>
-    );
-}
+  const images = [
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    V
+  ];
+
+  const [lightboxImage, setLightboxImage] = useState(null);
+
+  const openLightbox = (image) => {
+    setLightboxImage(image);
+  };
+
+  const closeLightbox = () => {
+    setLightboxImage(null);
+  };
+
+  return (
+    <div id="gallery" className="bg-gradient-to-b from-[#29153D] to-[#000000] ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 py-8 mx-4">
+        {images.map((image, index) => (
+          <div key={index} className="w-full p-2 border border-4 events_events-card__VuUy3  border-solid border-[#442F47] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 " >
+            <div
+              className="flex items-center justify-center cursor-pointer "
+              onClick={() => openLightbox(image)}
+            >
+              <img
+                src={image}
+                alt={`Image ${index}`}
+                className="w-323.8 h-243.64"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {lightboxImage && (
+  <div
+    className="fixed inset-0 flex items-center justify-center z-50"
+    onClick={closeLightbox}
+  >
+    <div className="bg-black bg-opacity-70 absolute inset-0"></div>
+    <div className="relative max-w-screen-md">
+      <img
+        src={lightboxImage}
+        alt="Lightbox Image"
+        className="max-w-full max-h-full"
+      />
+      <button
+        onClick={closeLightbox}
+        className="absolute top-2 right-2 text-white text-1xl p-2 bg-gray-800 opacity-25"
+      >
+        X
+      </button>
+    </div>
+  </div>
+)}
+
+
+    </div>
+  );
+};
 
 export default Gallery;
