89db9946c72d230357f9161aace95e7b7c2bd5a9
Astraxx04 <gagan200254@gmail.com>
2023-11-24 15:47:28 +0530

    Rulebooks Job

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -30,14 +30,14 @@ const Navbar = () => {
   return (
       <nav className="fixed top-0 left-0 right-0 bg-[#28143a] bg-opacity-80 flex justify-between items-center z-10 px-5 py-1 lg:py-1 h-8.2 lg:h-18">
         <div className="flex flex-wrap items-center justify-between mx-auto p-2 w-full md:w-auto">
-          <a href="/#landing" className="lg:hidden md:hidden flex items-stretch justify-between">
+          <a href="/" className="lg:hidden md:hidden flex items-stretch justify-between">
             <img src={catalysis} className="h-8" alt="Catalysis Logo" />
           </a>
         <ion-icon onClick={toggleNavIcon} name={navIcon} class="text-white cursor-pointer text-3xl md:hidden lg:hidden"></ion-icon>
       <div class={`w-full md:block md:w-auto ${isDropdownOpen ? 'block' : 'hidden'}`} id="navbar-default">
         <ul class="font-medium flex flex-col p-0 md:p-0 mt-4  md:flex-row lg:space-x-8 md:space-x-2 md:text-sm md:mt-0 md:border-0 ">
           <li>
-            <Link to="/#landing" onClick={() => handleItemClick("landing")} className="text-[#F4D9D9] lg:text-2xl md:text-lg font-bold cursor-pointer hover:text-[#B1F8F2] hover:underline focus:text-[#B1F8F2] focus:underline" aria-current="page">Home</Link>
+            <Link to="/" onClick={() => handleItemClick("landing")} className="text-[#F4D9D9] lg:text-2xl md:text-lg font-bold cursor-pointer hover:text-[#B1F8F2] hover:underline focus:text-[#B1F8F2] focus:underline" aria-current="page">Home</Link>
           </li>
           <li>
             <Link to="/#about" onClick={() => handleItemClick("about")} className="text-[#F4D9D9] lg:text-2xl md:text-lg font-bold cursor-pointer hover:text-[#B1F8F2] hover:underline focus:text-[#B1F8F2] focus:underline">About</Link>
