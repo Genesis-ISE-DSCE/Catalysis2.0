307d287e3d5a0d6ecb2602b7c78ef61ccb3e9393
Sayanur Rahman <66135369+eternalconflict@users.noreply.github.com>
2023-12-06 17:01:19 +0530

    Added current sponsors for Catalysis 2.0

--- frontend/src/components/Sponsors.jsx
+++ frontend/src/components/Sponsors.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-const radix = require('../assets/radix.png');
-const hashit = require('../assets/hashitenary.jpg');
-const breakPoint = require('../assets/breakPoint.jpg');
-const wealthPoint = require('../assets/wealthPoint.jpg');
-const smradix = require('../assets/smradix.png');
-const smhashit = require('../assets/smhashitenary.jpg');
-const smbreakPoint = require('../assets/smbreakPoint.jpg');
-const smwealthPoint = require('../assets/smwealthPoint.jpg');
+// const radix = require('../assets/radix.png');
+// const hashit = require('../assets/hashitenary.jpg');
+// const breakPoint = require('../assets/breakPoint.jpg');
+// const wealthPoint = require('../assets/wealthPoint.jpg');
+// const smradix = require('../assets/smradix.png');
+// const smhashit = require('../assets/smhashitenary.jpg');
+// const smbreakPoint = require('../assets/smbreakPoint.jpg');
+// const smwealthPoint = require('../assets/smwealthPoint.jpg');
+
+const khoros = require('../assets/khorosWhite.png');
+const alpha = require('../assets/alpha.jpeg')
+const theLegacyCloset = require('../assets/theLegacyCloset.jpeg')
+const smkhoros = require('../assets/smkhorosWhite.png');
+const smalpha = require('../assets/smalpha.jpeg')
+const smtheLegacyCloset = require('../assets/smtheLegacyCloset.jpeg')
 
 
 const Sponsors = () => {
@@ -18,30 +25,30 @@ const Sponsors = () => {
 
     return(
         <div id = "sponsors" className="bg-gradient-to-b from-[#29153D] to-[#000000]">
-            <h1 className="font-bold text-[#E5F2FF] text-center text-4xl md:text-4xl lg:text-5xl pt-12 pb-8">Past Sponsors</h1>
+            <h1 className="font-bold text-[#E5F2FF] text-center text-4xl md:text-4xl lg:text-5xl pt-12 pb-8">Sponsors</h1>
             <div className="lg:flex pb-10 mx-6">
-                <img src={smradix} loading="lazy" className="lg:w-1/4 lg:h-[200px] lg:flex-row lg:mb-9 md:flex-col md:h-[200px] md:mt-9 md:mx-auto mx-auto flex-col pb-9 px-5 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 " style = {sponsorSize} rel="noreferrer" alt="Sponsor Logo" onLoad={(e)=>{
-                    e.target.src = radix
+                <img src={smkhoros} loading="lazy" className="lg:w-1/4 lg:h-[200px] lg:flex-row lg:mb-9 md:flex-col md:h-[200px] md:mt-9 md:mx-auto mx-auto flex-col pb-9 px-5 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 " style = {sponsorSize} rel="noreferrer" alt="Sponsor Logo" onLoad={(e)=>{
+                    e.target.src = khoros
                     e.target.removeAttribute("style");
                 }}/> 
 
-                <img src={smhashit} loading="lazy" className="lg:w-1/4 lg:h-[200px] lg:flex-row lg:mb-9 md:flex-col md:h-[200px] md:mt-9 md:mx-auto mx-auto flex-col pb-9 px-5
+                <img src={smalpha} loading="lazy" className="lg:w-1/4 lg:h-[200px] lg:flex-row lg:mb-9 md:flex-col md:h-[200px] md:mt-9 md:mx-auto mx-auto flex-col pb-9 px-5
                 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" style = {sponsorSize} rel="noreferrer" alt="Sponsor Logo" onLoad={(e)=>{
-                    e.target.src = hashit
+                    e.target.src = alpha
                     e.target.removeAttribute("style");
                 }}/>   
 
-                <img src={smbreakPoint} loading="lazy" className="lg:w-1/4 lg:h-[200px] lg:flex-row lg:mb-9 md:flex-col md:h-[200px] md:mt-9 md:mx-auto mx-auto flex-col pb-9 px-5
+                <img src={smtheLegacyCloset} loading="lazy" className="lg:w-1/4 lg:h-[200px] lg:flex-row lg:mb-9 md:flex-col md:h-[200px] md:mt-9 md:mx-auto mx-auto flex-col pb-9 px-5
                 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" style = {sponsorSize} rel="noreferrer" alt="Sponsor Logo" onLoad={(e)=>{
-                    e.target.src = breakPoint
+                    e.target.src = theLegacyCloset
                     e.target.removeAttribute("style");
                 }}/> 
 
-                <img src={smwealthPoint} loading="lazy" className="lg:w-1/4 lg:h-[200px] lg:flex-row lg:mb-9 md:flex-col md:h-[200px] md:mt-9 md:mx-auto mx-auto flex-col pb-9 px-5
+                {/* <img src={smwealthPoint} loading="lazy" className="lg:w-1/4 lg:h-[200px] lg:flex-row lg:mb-9 md:flex-col md:h-[200px] md:mt-9 md:mx-auto mx-auto flex-col pb-9 px-5
                 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"  style = {sponsorSize} rel="noreferrer" alt="Sponsor Logo" onLoad={(e)=>{
                     e.target.src = wealthPoint
                     e.target.removeAttribute("style");
-                }}/> 
+                }}/>  */}
                
             </div>
         </div>
