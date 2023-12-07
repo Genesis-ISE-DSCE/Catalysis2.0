bb0430a5bfcec235e9fb4fd294fcd05eb226246a
Janesh7 <janeshwalia273@gmail.com>
2023-12-07 23:45:47 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 
-const connectDB = require("./database/connect");
+const connectDB = require('./database/connect');
 const registerRoute = require('./routes/registerRoute');
-const massMailerRoute = require("./routes/massMailRoute")
+const massMailerRoute = require('./routes/massMailRoute');
 
 require('dotenv').config();
-const cors = require("cors");
+const cors = require('cors');
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://isecatalysis.in',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/v1', registerRoute);
@@ -17,14 +23,13 @@ app.use('/api/v1', massMailerRoute);
 const port = process.env.PORT || 5000;
 
 const startServer = async () => {
-    try {
-      await connectDB(process.env.MONGO_URI);
-      console.log("Database connection established successfully...");
-      app.listen(port, console.log(`Server started on port ${port}...`));
-    } catch (err) {
-      console.log(err);
-    }
-  
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log('Database connection established successfully...');
+    app.listen(port, console.log(`Server started on port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 startServer();
