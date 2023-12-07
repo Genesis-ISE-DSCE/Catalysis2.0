2edcd85ed644476b12ffe28626a63447eda2465f
Saakshi-Shenoy <shenoy.saakshi910@gmail.com>
2023-12-07 20:26:35 +0530

    Schedule done

--- frontend/src/components/Schedule.jsx
+++ frontend/src/components/Schedule.jsx
@@ -8,15 +8,16 @@ import Event3 from "../assets/Event3.png";
 
   return (
       <>
-      <div className="bg-gradient-to-b from-[#29153D] to-[#000000] pb-20 pt-7" >
-      <div className="h-96 sm:h-120 xl:h-140 2xl:h-180">
+      
+      <div className="bg-gradient-to-b from-[#29153D] to-[#000000] pb-10 pt-7" >
+      <div className="h-screen">
       <h1 className="font-bold text-[#E5F2FF] text-center text-4xl md:text-4xl lg:text-5xl">
         Schedule
       </h1>
-      <Carousel slide={false} className="mt-2 py-5 ">
-        <img src={Event1} alt="day1" className="max-h-full 2xl:w-1/3 2xl:h-auto"/> 
-        <img src={Event2} alt="day2" className="max-h-full 2xl:w-1/3 2xl:h-auto"/>
-        <img src={Event3} alt="day3" className="max-h-full 2xl:w-1/3 2xl:h-auto"/>
+      <Carousel slide={false}>
+        <img src={Event1} alt="day1" className="h-3/4 2xl:w-1/2 sm:w-1/2 2xl:h-auto pb-10"/> 
+        <img src={Event2} alt="day2" className="h-3/4 2xl:w-1/2 2xl:h-auto"/>
+        <img src={Event3} alt="day3" className="h-3/4 2xl:w-1/2 2xl:h-auto"/>
       </Carousel>
     </div>
   </div>
