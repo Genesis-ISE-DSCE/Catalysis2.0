73169f80eaa71001d87cc9edc246b2931f330be5
Akash Sil <108827497+Orange-Tofu@users.noreply.github.com>
2023-12-07 23:43:14 +0530

    added top margin to Schedule and reduced gap

--- frontend/src/components/Schedule.jsx
+++ frontend/src/components/Schedule.jsx
@@ -11,14 +11,19 @@ const Schedule = () => {
 
       <div className="bg-gradient-to-b from-[#29153D] to-[#000000] pb-10 pt-7" >
         <div className="h-screen">
-          <h1 className="font-bold text-[#E5F2FF] text-center text-4xl md:text-4xl lg:text-5xl">
+          <h1 className="font-bold text-[#E5F2FF] text-center text-4xl pt-5 md:text-4xl lg:text-5xl">
             Schedule
           </h1>
-          <Carousel slide={true}>
+          <Carousel slide={true} className="carousel-style" >
             <img src={Event1} alt="day1" className="h-3/4 2xl:w-1/2 sm:w-1/2 2xl:h-auto pb-10" />
             <img src={Event2} alt="day2" className="h-3/4 2xl:w-1/2 2xl:h-auto" />
             <img src={Event3} alt="day3" className="h-3/4 2xl:w-1/2 2xl:h-auto" />
           </Carousel>
+          <style>{`
+              .carousel-style {
+                margin-top: -5rem;
+              }
+            `}</style>
         </div>
       </div>
     </>
