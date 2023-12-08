a7af577a430846eaeb01721eaa90c811a257f0f0
Janesh7 <janeshwalia273@gmail.com>
2023-12-08 08:40:35 +0530

    mass mail

--- backend/controllers/massMailer.js
+++ backend/controllers/massMailer.js
@@ -56,57 +56,68 @@ const transporter = nodemailer.createTransport({
 });
 
 const mailAll = async (req, res) => {
-    try {
-        console.log(req.body);
-        const { user, subject, body, event, attachmentDocs } = req.body;
-
-        if (user === process.env.USER_AUTH) {
-            let users;
-
-            if (event === 'All') {
-                users = await Registration.find();
-            } else {
-                users = await Registration.find({ event: event });
-            }
-
-            let pdfAttachment = null;
-
-            if(attachmentDocs != "") {
-                
-                const pdfAttachment = {
-                    contentType: "application/pdf",
-                };
-
-                for (let attach of attachmentDocsPath) {
-                    if(attach.filename === attachmentDocs){
-                        pdfAttachment.filename = attach.filename;
-                        pdfAttachment.path = attach.path;
-                    }
-                }
-            }
-
-            const emails = users.map(user => user.email);
-            const uniqueEmail = [...new Set(emails)];
-
-            for (const email of uniqueEmail) {
-                const mailOptions = {
-                    from: process.env.MAIL_USER,
-                    to: email,
-                    subject: subject,
-                    text: body,
-                    attachments: pdfAttachment,
-                };
-                await transporter.sendMail(mailOptions);
-            }
-
-            res.status(200).send('Emails sent successfully');
-        } else {
-            res.status(401).send('Unauthorized Access'); // 401 for unauthorized access
-        }
-    } catch (error) {
-        console.error('Error sending emails:', error);
-        res.status(500).send('Internal Server Error');
+    // Handle CORS preflight requests
+    if (req.method === 'OPTIONS') {
+      res.setHeader('Access-Control-Allow-Origin', 'https://isecatalysis.in');
+      res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers',
+   
+  'Content-Type, Authorization');
+      res.status(200).send();
+      return;
     }
-};
-
-module.exports = { mailAll };
+  
+    // Handle POST requests
+    try {
+      const { user, subject, body, event, attachmentDocs } = req.body;
+  
+      if (user === process.env.USER_AUTH) {
+        let users;
+  
+        if (event === 'All') {
+          users = await Registration.find();
+        } else {
+          users = await Registration.find({ event: event });
+        }
+  
+        let pdfAttachment = null;
+  
+        if (attachmentDocs !== "") {
+          const pdfAttachment = {
+            contentType: "application/pdf",
+          };
+  
+          for (const attach of attachmentDocsPath) {
+            if (attach.filename === attachmentDocs) {
+              pdfAttachment.filename = attach.filename;
+              pdfAttachment.path = attach.path;
+              break;
+            }
+          }
+        }
+  
+        const emails = users.map(user => user.email);
+        const uniqueEmail = [...new Set(emails)];
+  
+        for (const email of uniqueEmail) {
+          const mailOptions = {
+            from: process.env.MAIL_USER,
+            to: email,
+            subject: subject,
+            text: body,
+            attachments: pdfAttachment,
+          };
+          await transporter.sendMail(mailOptions);
+        }
+  
+        res.status(200).send('Emails sent successfully');
+      } else {
+        res.status(401).send('Unauthorized Access');
+      }
+    } catch (error) {
+      console.error('Error sending emails:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  };
+  
+  module.exports = { mailAll };
